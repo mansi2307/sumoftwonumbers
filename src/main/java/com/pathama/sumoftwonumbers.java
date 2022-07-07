c63940ca2d97a905c6b4009d3a392f83e8e59624
Mansi Pathak <pathakmansi237@gmail.com>
2022-07-07 13:11:03 -0500

    adding files

---
+++ src/main/java/com/pathama/sumoftwonumbers.java
@@ -0,0 +1,17 @@
+package com.pathama;
+
+/**
+ * Hello world!
+ *
+ */
+public class sumoftwonumbers 
+{
+    public static void main(String args[])
+    {
+      int x = Integer.parseInt(args[0]);
+      int y = Integer.parseInt(args[1]);    
+      int sum = x + y;
+
+      System.out.println("Sum of the numbers is:" +sum);
+    }
+}
